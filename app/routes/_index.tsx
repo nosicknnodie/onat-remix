9540be3654d67065389824210d4a8d7b129fb118
nosicknnodie <nosicknnodie@naver.com>
2025-04-25 23:05:04 +0900

    fix: cicd test

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,7 +2,7 @@ export default function Index() {
   return (
     <div>
       <div className="max-w-5xl w-full items-start font-mono text-sm space-y-4">
-        <span>test 8 cicd test</span>
+        <span>test 9 cicd test</span>
       </div>
     </div>
   );
