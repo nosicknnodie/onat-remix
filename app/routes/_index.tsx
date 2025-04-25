48f38ad3e5bfc98044adf25583eeb72f3b0265b2
nosicknnodie <nosicknnodie@gmail.com>
2025-04-25 18:20:31 +0900

    test: cicd test

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,7 +2,7 @@ export default function Index() {
   return (
     <div>
       <div className="max-w-5xl w-full items-start font-mono text-sm space-y-4">
-        <span>asdfasdfasdf</span>
+        <span>test2</span>
       </div>
     </div>
   );
