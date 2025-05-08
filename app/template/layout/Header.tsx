348254d9205a2dccc58b876be170a91f8871d935
nosicknnodie <nosicknnodie@gmail.com>
2025-05-08 15:27:39 +0900

    test: cicd

--- app/template/layout/Header.tsx
+++ app/template/layout/Header.tsx
@@ -25,7 +25,7 @@ const Header = () => {
         <div>
           <Link to={"/"} className="px-2 py-0.5">
             <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
-              ONSOA
+              ONSOA 1
             </span>
           </Link>
         </div>
